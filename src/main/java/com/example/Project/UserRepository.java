7b43e7aca74fcca728b8e94a34602db9fbf830ca
Dany DeCarvalho <monemail@fournisseur.com>
2022-05-22 23:36:32 +0200

    mardi cest al fin

--- src/main/java/com/example/Project/UserRepository.java
+++ src/main/java/com/example/Project/UserRepository.java
@@ -3,9 +3,10 @@ package com.example.Project;
 import com.example.Project.Models.Order;
 import com.example.Project.Models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
 }
